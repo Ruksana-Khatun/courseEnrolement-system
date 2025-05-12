5dd7a2c92c7cd742cad620e6860ea393b25c7508
Ruksana Khatun <iitianruksana@gmail.com>
2025-05-13 01:35:38 +0530

    added post route for createCourse

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,11 +1,23 @@
-import {Router} from 'express';
-import { getAllCourses, getLecturesCourseById} from '../controllers/course.controller.js';
-import { isLoggedIn } from '../middlewares/auth.middlewares.js';
 
+import {Router} from 'express';
+import { getAllCourses, getLecturesCourseById, createCourse,updateCourse,removeCourse} from '../controllers/course.controller.js';
+import { isLoggedIn } from '../middlewares/auth.middlewares.js';
+import upload from "../middlewares/multer.middleware.js";
 
 const router = Router();
-router.route('/').get(getAllCourses);
-router.route('/:id').get(isLoggedIn,getLecturesCourseById);
+router.route('/')
+.get(getAllCourses)
+.post(
+    upload.single("thumbnail"),
+    isLoggedIn,
+    createCourse
+)
+
+
+router.route('/:id')
+.get(isLoggedIn,getLecturesCourseById)
+.put(isLoggedIn,updateCourse)
+.delete(isLoggedIn,removeCourse);
 
 
 export default router
