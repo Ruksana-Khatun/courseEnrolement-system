43ad10f0446180f54b339e040dd1a4af25aa8695
Ruksana Khatun <iitianruksana@gmail.com>
2025-05-13 04:49:11 +0530

    completed only admin can add lecture

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -144,10 +144,65 @@ const removeCourse = async (req, res,next) => {
     return next(new ErrorApp(error.message, 500));
   }
 }
+const addLectureToCourseBYid = async (req, res,next) => {
+  try{
+    const { title, description} = req.body;
+  const id = req.params.id;
+  if (!title || !description) {
+    return next(new ErrorApp("Please provide all required fields", 400));
+  }
+  const course = await Course.findById(id);
+  if (!course) {
+    return next(new ErrorApp("Course with given id does not exist", 404));
+  }
+  const lectureData = {
+    title,
+    description,
+    lecture:{}
+  }
+  if (req.file) {
+    try {
+      const filePath = path.join(req.file.destination, req.file.filename);
+      const normalizedPath = filePath.replace(/\\/g, '/');
+
+      const result = await cloudinary.uploader.upload(normalizedPath, {
+        folder: "lectures",
+        resource_type: "video",
+        width: 150,
+        crop: "scale",
+      });
+
+      if (result) {
+        lectureData.lecture.public_id = result.public_id;
+        lectureData.lecture.secure_url = result.secure_url;
+
+        fs.unlinkSync(normalizedPath);
+      }
+    } catch (uploadError) {
+      console.error("Cloudinary Upload Error:", uploadError);
+      return next(new ErrorApp(uploadError.message, 500));
+    }
+  }
+  course.lectures.push(lectureData);
+  course.numberOfLectures = course.lectures.length;
+  await course.save();
+  res.status(200).json({
+    success: true,
+    message: "Lecture added successfully",
+    course,
+  });
+
+  }catch(e){
+    return next(new ErrorApp(e.message, 500));
+  }
+  
+  
+}
 export{
     getAllCourses,
     getLecturesCourseById,
     createCourse,
     updateCourse,
-    removeCourse
+    removeCourse,
+    addLectureToCourseBYid
 }
