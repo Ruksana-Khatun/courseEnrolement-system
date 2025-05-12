ba365719acdca7fbaf75e11990ce69dbadc5b11d
Ruksana Khatun <iitianruksana@gmail.com>
2025-05-13 03:04:06 +0530

    completed Admin AuthrizedRoles for create uptodate delete course

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,23 +1,29 @@
 
 import {Router} from 'express';
 import { getAllCourses, getLecturesCourseById, createCourse,updateCourse,removeCourse} from '../controllers/course.controller.js';
-import { isLoggedIn } from '../middlewares/auth.middlewares.js';
+import { isLoggedIn,authorizedRoles } from '../middlewares/auth.middlewares.js';
 import upload from "../middlewares/multer.middleware.js";
-
 const router = Router();
 router.route('/')
 .get(getAllCourses)
 .post(
     upload.single("thumbnail"),
     isLoggedIn,
+    authorizedRoles("admin"),
     createCourse
 )
 
 
 router.route('/:id')
 .get(isLoggedIn,getLecturesCourseById)
-.put(isLoggedIn,updateCourse)
-.delete(isLoggedIn,removeCourse);
+.put(
+    isLoggedIn,
+   authorizedRoles("admin"),
+    updateCourse
+)
+.delete(isLoggedIn,
+    authorizedRoles("admin"), 
+    removeCourse);
 
 
 export default router
