2af7623e43e17beb351fbcd49e9b54205fc20cba
Ruksana Khatun <iitianruksana@gmail.com>
2025-05-13 01:36:27 +0530

    fixed some code

--- server.js
+++ server.js
@@ -3,25 +3,18 @@ dotenv.config();
 import app from "./app.js";
 import connectionToDB from "./config/dbConection.js";
 import cloudinary from './config/cloudinary.config.js';
-
-
 const PORT = process.env.PORT || 5000;
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
 });
-
-// Start server function
 const startServer = async () => {
     try {
      
         await connectionToDB();
         console.log('Connected to MongoDB');
-
-        // Start Express server
         app.listen(PORT, () => {
             console.log(`Server is running in mode on port ${PORT}`);
         });
