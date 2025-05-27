eefb271a67d2485bf71baa7f4d1c6aefafcd6137
Ruksana Khatun <iitianruksana@gmail.com>
2025-05-28 02:07:31 +0530

    intall payment packege

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,18 +1,27 @@
 import jwt from "jsonwebtoken";
 import ErrorApp from "../utils/error.utils.js";
+import User from "../models/user.model.js";
 const isLoggedIn= async(req,res,next)=>{
     try{
         const token = req.cookies?.token;
     if(!token){
         return next(new ErrorApp('unauthenticated, please login again ',401))
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) {
           console.log("JWT Error:", err.message); 
           return res.status(401).json({ success: false, message: "Invalid or expired token" });
         }
-        req.user = decoded;
-        next();
+        const user = await User.findById(decoded.id); // ✅ fetch full user from DB
+
+      if (!user) {
+        return next(new ErrorApp("User not found", 401));
+      }
+
+      req.user = user; // ✅ Attach full user to request
+      next();
+        // req.user = decoded;
+        // next();
       }); 
      
     }catch{
@@ -20,6 +29,7 @@ const isLoggedIn= async(req,res,next)=>{
     }
    
 }
+
  const authorizedRoles=(...roles)=> {
         return (req, res, next) => {
             const currentUserRole = req.user.role;
